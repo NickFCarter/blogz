c675a93a9141ad98455b4e6f05d1733b82b7696d
Nick Carter <nickfcarter95@gmail.com>
2018-04-13 17:00:56 -0500

    added basic functionality to newpost and mainBlog pages

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, redirect, render_template
-from flask_sqlalchemy import flask_sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:launchcode@localhost:8889/build-a-blog'
@@ -10,11 +10,32 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(150))
-    content = db.Column(db.String(1000))
+    body = db.Column(db.String(1000))
 
-    def __init__(self, title, content):
+    def __init__(self, title, body):
         self.title = title
-        self.content = content
+        self.body = body
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    #TODO - Display blogs and titles on main page
+    blogs = Blog.query.all()
+    return render_template('mainBlogPage.html', title="Blog Page", blogs=blogs)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def add_post():
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        new_post = Blog(title, body)
+        db.session.add(new_post)
+        db.session.commit()
+        blogs = Blog.query.all()
+        return render_template('mainBlogPage.html', title="Blog Page", blogs=blogs)
+    
+    else:
+        return render_template('newpost.html', title="New Blog")
 
 
 if __name__ == '__main__':
