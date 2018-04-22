56e713aa4a06b5b6119280a20cdbf4959dc61365
Nick Carter <nickfcarter95@gmail.com>
2018-04-22 12:42:26 -0500

    worked on templates

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:launchcode@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'b4308cvnh4e09z7jas'
 
 class Blog(db.Model):
 
